6ea9e1a22a4633c0a1c200036436eb0b91b51017
Guru Prasad M <gururprasad123@gmail.com>
2025-05-18 16:10:35 +0530

    Learn a program on game of guess the number
    develop a program to guess a number that the computer has decided or chosen randomly

---
+++ guess.py
@@ -0,0 +1,14 @@
+import random as ran
+n=ran.randint(1,101)
+while True:
+    a=int(input("Enter any number between 1 and 100:"))
+    if a<n:
+        print("Your guess is low")
+        continue
+    elif a>n:
+        print("Your guess is high")
+        continue
+    else:
+        print("You are correct")
+        break
+    
